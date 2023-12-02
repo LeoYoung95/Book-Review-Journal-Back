3846242dbe114bb15dad1b114b2db4febcae4cd2
yangxu95 <yang.xu6@northeastern.edu>
2023-12-01 20:01:14 -0800

    Update settings

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ import express from 'express';
 import mongoose from "mongoose";
 import cors from 'cors';
 import session from "express-session";
-import "dotenv/config";
-import UsersRoutes from "./users/routes";
-import ReviewsRoutes from "./reviews/routes";
-import BooksRoutes from "./books/routes";
+import UsersRoutes from "./users/routes.js";
+import ReviewsRoutes from "./reviews/routes.js";
+import BooksRoutes from "./books/routes.js";
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
 mongoose.connect(CONNECTION_STRING).then(r => console.log("Connected to MongoDB"));
@@ -41,4 +43,10 @@ UsersRoutes(app);
 ReviewsRoutes(app);
 BooksRoutes(app);
 
-app.listen(process.env.PORT || 4000);
+app.get('/', (req, res) => {
+    res.send('BRJ Server is running on port 4000');
+});
+
+app.listen(process.env.PORT || 4000, () => {
+    console.log("BRJ Server is running on port 4000");
+});
