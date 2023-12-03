0fdf7fda462543bf98495c46d75c1dbb58c567d6
yangxu95 <yang.xu6@northeastern.edu>
2023-12-02 20:21:38 -0800

    update model names

--- users/model.js
+++ users/model.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 import schema from "./schema.js";
-const UserModel = mongoose.model("users", schema);
+const UserModel = mongoose.model("User", schema);
 export default UserModel;
 
