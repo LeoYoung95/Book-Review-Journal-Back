0e5bb4656769243954239b1b63a789aebf440bbd
Jiawen Gu <gu.jiawen@northeastern.edu>
2023-12-04 23:43:53 -0800

    change profilePicId range

--- users/schema.js
+++ users/schema.js
@@ -36,7 +36,7 @@ const userSchema = new mongoose.Schema({
         profilePicId: [{
             type: String,
             required: false,
-            enum: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+            enum: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
         }],
     },
 
